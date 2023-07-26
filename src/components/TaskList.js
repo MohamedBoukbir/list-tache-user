e633eaee995475439860b540b144eef3cfaa3251
[your <mohamed.boukbir@uit.ac.ma>
2023-07-26 15:12:36 +0100

    commit 3

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import './TaskList.css'; // Importe le fichier CSS
 
 const TaskList = () => {
   const { userId } = useParams();
   const [tasks, setTasks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [tasksPerPage] = useState(10); // Nombre de tâches par page, tu peux ajuster ce nombre selon tes besoins
+  // Calculer le nombre total de pages
+  const totalPages = Math.ceil(tasks.length / tasksPerPage);
+
+  // Fonction pour changer de page
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
     // Appeler l'API REST pour récupérer les tâches de l'utilisateur sélectionné
@@ -13,15 +23,55 @@ const TaskList = () => {
       .then((data) => setTasks(data));
   }, [userId]);
 
+    // Calculer l'index de la première et de la dernière tâche sur la page actuelle
+    const indexOfLastTask = currentPage * tasksPerPage;
+    const indexOfFirstTask = indexOfLastTask - tasksPerPage;
+    const currentTasks = tasks.slice(indexOfFirstTask, indexOfLastTask);
+
   return (
-    <div>
-      <h2>Tâches de l'Utilisateur</h2>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
-        ))}
-      </ul>
-    </div>
+    // <div>
+    //   <h2>Tâches de l'Utilisateur</h2>
+    //   <ul>
+    //     {tasks.map((task) => (
+    //       <li key={task.id}>{task.title}</li>
+    //     ))}
+    //   </ul>
+    // </div>
+    <div className="task-list">
+    <h2 className='titleh2'>Tâches de l'Utilisateur</h2>
+    {currentTasks.length > 0 ? (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Tâche</th>
+            <th>Statut</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.title}</td>
+              <td>{task.completed ? 'Terminée' : 'En cours'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    ) : (
+      <p className="no-tasks">Aucune tâche disponible pour cet utilisateur.</p>
+    )}
+    {/* /// */}
+      {totalPages > 1 && (
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+            <button key={page} onClick={() => handlePageChange(page)}>
+              {page}
+            </button>
+          ))}
+        </div>
+      )}
+  </div>
   );
 };
 
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [userPerPage] = useState(5);
+  // Calculer le nombre total de pages
+  const totalPages = Math.ceil(users.length / userPerPage);
+
+  // Fonction pour changer de page
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
 
   useEffect(() => {
     // Appeler l'API REST pour récupérer la liste des utilisateurs
@@ -10,17 +20,61 @@ const UserList = () => {
       .then((response) => response.json())
       .then((data) => setUsers(data));
   }, []);
+   // Calculer l'index de la première et de la dernière tâche sur la page actuelle
+   const indexOfLastUser = currentPage * userPerPage;
+   const indexOfFirstUser = indexOfLastUser - userPerPage;
+   const currentUser = users.slice(indexOfFirstUser, indexOfLastUser);
+
 
   return (
-    <div>
-      <h2>Liste des Utilisateurs</h2>
-      <ul>
+    <div className="task-list">
+      <h2 className='titleh2'>Liste des Utilisateurs</h2>
+      {/* <ul>
         {users.map((user) => (
+
           <li key={user.id}>
-            <Link to={`/tasks/${user.id}`}>{user.name}</Link>
+
+            <Link to={`/tasks/${user.id}`}>{user.name}</Link> 
           </li>
+
+
         ))}
-      </ul>
+      </ul> */}
+       {/* // table // */}
+          {currentUser.length > 0 ? (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>username</th>
+            <th>email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentUser.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email }</td>
+              <td key={user.id}>   <Link to={`/tasks/${user.id}`}> Liste des tâches</Link>   </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    ) : (
+      <p className="no-tasks">Aucune tâche disponible pour cet utilisateur.</p>
+    )}
+     {/* // end table // */}
+     {totalPages > 1 && (
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+            <button key={page} onClick={() => handlePageChange(page)}>
+              {page}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
